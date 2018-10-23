568df6fc046230bf40d8e2044e2a52668f7260e9
SangHyunKim <tkdgus8015@naver.com>
2018-10-23 16:12:11 +0900

    MVC로 패키지 분할

--- app/src/main/java/com/example/user/teamproject/view/MainActivity.java
+++ app/src/main/java/com/example/user/teamproject/view/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.user.teamproject;
+package com.example.user.teamproject.view;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -6,6 +6,8 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.example.user.teamproject.R;
 
 public class MainActivity extends AppCompatActivity {
 
